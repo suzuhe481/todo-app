c9b645bf2ff493494f41da4fb9cdc50ce0bd0c53
Hector <baseballhector@gmail.com>
2023-06-22 16:15:52 -0400

    Function to create todo item.

---
+++ src/components/todo/todoItem.js
@@ -0,0 +1,37 @@
+// Creates div containing a single todo item.
+const todoItem = (todo) => {
+  console.log("run");
+  console.log(todo);
+
+  const todoItem = document.createElement("div");
+  todoItem.classList.add("todo-item");
+
+  const todoTitle = document.createElement("div");
+  const todoDescription = document.createElement("div");
+  const todoDate = document.createElement("div");
+  const todoPriority = document.createElement("div");
+
+  todoTitle.classList.add("todo-title");
+  todoDescription.classList.add("todo-description");
+  todoDate.classList.add("todo-date");
+  todoPriority.classList.add("todo-priority");
+
+  todoTitle.innerHTML = todo.title;
+  todoDescription.innerHTML = todo.description;
+
+  var month = todo.dueDate.toLocaleString("default", { month: "short" });
+  var day = todo.dueDate.getDate();
+  var year = todo.dueDate.getFullYear();
+  todoDate.innerHTML = month + " " + day + ", " + year;
+
+  todoPriority.innerHTML = todo.highPriority;
+
+  todoItem.appendChild(todoTitle);
+  todoItem.appendChild(todoDescription);
+  todoItem.appendChild(todoDate);
+  todoItem.appendChild(todoPriority);
+
+  return todoItem;
+};
+
+export default todoItem;
