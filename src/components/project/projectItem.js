e0d925a5fb33e7bb567eaa828e931c24fa867489
Hector <baseballhector@gmail.com>
2023-06-22 18:34:40 -0400

    Component for project item

---
+++ src/components/project/projectItem.js
@@ -0,0 +1,10 @@
+// Creates empty project with given name.
+const projectItem = (projectName) => {
+  var project = {
+    name: projectName,
+  };
+
+  return project;
+};
+
+export default projectItem;
