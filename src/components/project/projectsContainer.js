55f2ba129e8473085e9d7606d5505522c8f9af29
Hector <baseballhector@gmail.com>
2023-06-24 16:30:40 -0400

    Refactored to use localStorage instead of passed parameter

--- src/components/project/projectsContainer.js
+++ src/components/project/projectsContainer.js
@@ -2,15 +2,18 @@ import createProjectForm from "./projectForm";
 
 import switchProjectHandler from "./switchProjectHandler";
 
-const projectsContainer = (projects) => {
+const projectsContainer = () => {
   const projectsContainer = document.createElement("div");
   projectsContainer.id = "projects-container";
 
+  // Get variables from localStorage
+  var projectsList = JSON.parse(localStorage.getItem("projectsList"));
+
   // Appends each project as an item to the container.
-  for (var i = 0; i < projects.length; i++) {
+  for (var i = 0; i < projectsList.length; i++) {
     const projectItem = document.createElement("div");
     projectItem.dataset.value = i;
-    projectItem.innerHTML = projects[i].name;
+    projectItem.innerHTML = projectsList[i].name;
 
     projectItem.addEventListener("click", switchProjectHandler);
 
