1b8461ec9412fe6724610f65dcdd6cd6f7c42e58
Hector <baseballhector@gmail.com>
2023-06-24 16:26:42 -0400

    Clears form on submit. Switches project.

--- src/components/todo/addTodoItem.js
+++ src/components/todo/addTodoItem.js
@@ -1,4 +1,4 @@
-import createTodoItem from "./todoItem";
+import { displaySwitchedProject } from "../../modules/display";
 
 const addTodoItem = () => {
   event.preventDefault();
@@ -7,11 +7,17 @@ const addTodoItem = () => {
   var projectsList = JSON.parse(localStorage.getItem("projectsList"));
   var currentProject = localStorage.getItem("currentProject");
 
+  // Retrieve form values.
   var title = document.getElementById("todo-title").value;
   var desc = document.getElementById("todo-desc").value;
   var dueDate = document.getElementById("todo-date").value;
   var priority = document.getElementById("todo-priority").checked;
 
+  // Clear form.
+  var form = document.getElementById("todo-form");
+  form.reset();
+
+  // Store form values in todo object.
   var todo = {
     title: title,
     description: desc,
@@ -24,6 +30,8 @@ const addTodoItem = () => {
 
   // Update localStorage
   localStorage.setItem("projectsList", JSON.stringify(projectsList));
+
+  displaySwitchedProject();
 };
 
 export default addTodoItem;
