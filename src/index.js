0a7779401a6cb898a4d6b3f94a75ed96fc347562
Hector <baseballhector@gmail.com>
2023-06-24 19:15:14 -0400

    Commented out dummy data

--- src/index.js
+++ src/index.js
@@ -7,42 +7,40 @@ import { display } from "./modules/display";
 if (!localStorage.getItem("projectsList")) {
   var projectsList = [];
 
-  var project1 = createProjectItem("Default Project 1");
-  var project2 = createProjectItem("Default Project 2");
+  // Dummy data
+  //   var project1 = createProjectItem("Default Project 1");
+  //   var project2 = createProjectItem("Default Project 2");
 
-  projectsList.push(project1);
-  projectsList.push(project2);
+  //   projectsList.push(project1);
+  //   projectsList.push(project2);
 
-  var todo1 = {
-    title: "First title",
-    description: "Description of first todo",
-    dueDate: new Date("2023, 1, 5"),
-    highPriority: 1,
-  };
+  //   var todo1 = {
+  //     title: "First title",
+  //     description: "Description of first todo",
+  //     dueDate: new Date("2023, 1, 5"),
+  //     highPriority: 1,
+  //   };
 
-  var todo2 = {
-    title: "A second title",
-    description: "More stuff to do",
-    dueDate: new Date("2023, 5, 11"),
-    highPriority: false,
-  };
+  //   var todo2 = {
+  //     title: "A second title",
+  //     description: "More stuff to do",
+  //     dueDate: new Date("2023, 5, 11"),
+  //     highPriority: false,
+  //   };
 
-  var todo3 = {
-    title: "Another todo",
-    description: "Even more stuff",
-    dueDate: new Date("2023, 7, 22"),
-    highPriority: true,
-  };
+  //   var todo3 = {
+  //     title: "Another todo",
+  //     description: "Even more stuff",
+  //     dueDate: new Date("2023, 7, 22"),
+  //     highPriority: true,
+  //   };
 
-  var todoList1 = [];
-  todoList1.push(todo1);
-  todoList1.push(todo2);
+  //   projectsList[0].todoList.push(todo1);
+  //   projectsList[0].todoList.push(todo2);
 
-  var todoList2 = [];
-  todoList2.push(todo3);
+  //   projectsList[1].todoList.push(todo3);
 
-  projectsList[0].todoList = todoList1;
-  projectsList[1].todoList = todoList2;
+  // End of dummy data
 
   localStorage.setItem("projectsList", JSON.stringify(projectsList));
   localStorage.setItem("currentProject", 0);
