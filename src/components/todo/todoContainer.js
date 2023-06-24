3d6f7d9fe236d787a234815c78b55f3f4ba8a0b7
Hector <baseballhector@gmail.com>
2023-06-24 19:13:09 -0400

    Checks for empty projects

--- src/components/todo/todoContainer.js
+++ src/components/todo/todoContainer.js
@@ -4,10 +4,15 @@ const todoContainer = () => {
   var projectsList = JSON.parse(localStorage.getItem("projectsList"));
   var currentProject = localStorage.getItem("currentProject");
 
-  var todoList = projectsList[currentProject].todoList;
-
   const todoContainer = document.createElement("div");
   todoContainer.id = "todo-container";
+
+  // If a todolist exists for the currentProject, retrieve it.
+  if (projectsList[currentProject]) {
+    var todoList = projectsList[currentProject].todoList;
+  } else {
+    return todoContainer;
+  }
 
   // Appends each todo item to container.
   for (var i = 0; i < todoList.length; i++) {
