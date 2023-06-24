091ba18f9daf83225f37a93a60bcc358e8aba451
Hector <baseballhector@gmail.com>
2023-06-24 15:07:25 -0400

    Changes currentProject in localStorage

---
+++ src/components/project/switchProjectHandler.js
@@ -0,0 +1,13 @@
+import { displaySwitchedProject } from "../../modules/display";
+
+const switchProjectHandler = (event) => {
+  //  Retrieve project to switch to from element clicked.
+  var projectNumToSwitch = event.target.getAttribute("data-value");
+
+  //  Changes localStorage of variable "currentProject" to the project number clicked.
+  localStorage.setItem("currentProject", projectNumToSwitch);
+
+  displaySwitchedProject();
+};
+
+export default switchProjectHandler;
