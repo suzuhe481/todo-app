b23b92e0b90b3355509186d80f00555238cf4228
Hector <baseballhector@gmail.com>
2023-06-24 19:18:56 -0400

    Removed commented dummy data

--- src/modules/display.js
+++ src/modules/display.js
@@ -2,82 +2,6 @@ import createTodoForm from "../components/todo/todoForm";
 import createTodoContainer from "../components/todo/todoContainer";
 import createProjectsContainer from "../components/project/projectsContainer";
 
-// Dummy data
-
-// var projectsList = [];
-
-// var project1 = createProjectItem("Project 1");
-// var project2 = createProjectItem("Second project");
-// var project3 = createProjectItem("Last project");
-
-// projectsList.push(project1);
-// projectsList.push(project2);
-// projectsList.push(project3);
-
-// var todo1 = {
-//   title: "First title",
-//   description: "Description of first todo",
-//   dueDate: new Date("2023, 1, 5"),
-//   highPriority: 1,
-// };
-
-// var todo2 = {
-//   title: "A second title",
-//   description: "More stuff to do",
-//   dueDate: new Date("2023, 5, 11"),
-//   highPriority: false,
-// };
-
-// var todo3 = {
-//   title: "Another todo",
-//   description: "Even more stuff",
-//   dueDate: new Date("2023, 7, 22"),
-//   highPriority: true,
-// };
-
-// var todo4 = {
-//   title: "A fourth title",
-//   description: "Stuff for project 2",
-//   dueDate: new Date("2023, 5, 11"),
-//   highPriority: false,
-// };
-
-// var todo5 = {
-//   title: "More list",
-//   description: "Another todo item for project 2",
-//   dueDate: new Date("2023, 7, 22"),
-//   highPriority: true,
-// };
-
-// var todo6 = {
-//   title: "Todo on project 3",
-//   description: "Even more stuff",
-//   dueDate: new Date("2023, 7, 22"),
-//   highPriority: true,
-// };
-
-// Adding dummy data.
-// var todoList1 = [];
-// todoList1.push(todo1);
-// todoList1.push(todo2);
-// todoList1.push(todo3);
-// projectsList[0].todoList = todoList1;
-
-// var todoList2 = [];
-// todoList2.push(todo4);
-// todoList2.push(todo5);
-// projectsList[1].todoList = todoList2;
-
-// var todolist3 = [];
-// todolist3.push(todo6);
-// projectsList[2].todoList = todolist3;
-
-// Local Storage
-// localStorage.setItem("projectsList", JSON.stringify(projectsList));
-// localStorage.setItem("currentProject", 0);
-
-// End dummy data
-
 // Displays updated project names.
 const displayUpdatedProjects = () => {
   const sidebarElement = document.getElementById("side-bar");
@@ -88,8 +12,6 @@ const displayUpdatedProjects = () => {
   projectsContainerElement.remove();
 
   const projectContainer = createProjectsContainer();
-
-  console.log(projectContainer);
 
   sidebarElement.appendChild(projectContainer);
 };
