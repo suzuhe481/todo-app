be79220b621712f363892eacd3a9d0c4eb1a88e6
Hector <baseballhector@gmail.com>
2023-06-24 15:06:23 -0400

    Added event listener to project items

--- src/components/project/projectsContainer.js
+++ src/components/project/projectsContainer.js
@@ -1,5 +1,7 @@
 import createProjectForm from "./projectForm";
 
+import switchProjectHandler from "./switchProjectHandler";
+
 const projectsContainer = (projects) => {
   const projectsContainer = document.createElement("div");
   projectsContainer.id = "projects-container";
@@ -7,7 +9,10 @@ const projectsContainer = (projects) => {
   // Appends each project as an item to the container.
   for (var i = 0; i < projects.length; i++) {
     const projectItem = document.createElement("div");
+    projectItem.dataset.value = i;
     projectItem.innerHTML = projects[i].name;
+
+    projectItem.addEventListener("click", switchProjectHandler);
 
     projectsContainer.appendChild(projectItem);
   }
