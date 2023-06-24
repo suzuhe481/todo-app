93467f9c81d8a3592be808c40996295feaf0ebf4
Hector <baseballhector@gmail.com>
2023-06-24 19:10:37 -0400

    Adds new project to project list

---
+++ src/components/project/addProjectItem.js
@@ -0,0 +1,26 @@
+import createProjectItem from "./projectItem";
+import { displayUpdatedProjects } from "../../modules/display";
+
+const addProjectItem = () => {
+  event.preventDefault();
+
+  // Get variables from localStorage
+  var projectsList = JSON.parse(localStorage.getItem("projectsList"));
+
+  var projectName = document.getElementById("project-name").value;
+
+  // Clear form.
+  var form = document.getElementById("project-form");
+  form.reset();
+
+  var newProject = createProjectItem(projectName);
+
+  projectsList.push(newProject);
+
+  // Update localStorage
+  localStorage.setItem("projectsList", JSON.stringify(projectsList));
+
+  displayUpdatedProjects();
+};
+
+export default addProjectItem;
