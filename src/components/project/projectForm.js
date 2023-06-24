839a85fd5747d23f2ecd63d85810ee640ba66f8a
Hector <baseballhector@gmail.com>
2023-06-24 19:11:15 -0400

    Added button and wrapper div to project form.

--- src/components/project/projectForm.js
+++ src/components/project/projectForm.js
@@ -1,6 +1,11 @@
+import addProjectItem from "../project/addProjectItem";
+
 const createProjectForm = () => {
-  const projectForm = document.createElement("div");
-  projectForm.classList.add("form-row");
+  const form = document.createElement("form");
+  form.id = "project-form";
+  form.classList.add("form-row");
+
+  const nameRow = document.createElement("div");
 
   const nameLabel = document.createElement("label");
   nameLabel.for = "project-name";
@@ -11,10 +16,21 @@ const createProjectForm = () => {
   nameInput.id = "project-name";
   nameInput.name = "project-name";
 
-  projectForm.appendChild(nameLabel);
-  projectForm.appendChild(nameInput);
+  form.appendChild(nameLabel);
+  form.appendChild(nameInput);
 
-  return projectForm;
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.onclick = addProjectItem;
+  submitButton.innerHTML = "Submit";
+
+  nameRow.appendChild(nameLabel);
+  nameRow.appendChild(nameInput);
+
+  form.appendChild(nameRow);
+  form.appendChild(submitButton);
+
+  return form;
 };
 
 export default createProjectForm;
