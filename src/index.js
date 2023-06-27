b4f2dc78e7a04884ce7ac3926eb18350604632a2
Hector <baseballhector@gmail.com>
2023-06-27 14:14:25 -0400

    Declare undefined currentProject variable

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { display } from "./modules/display";
 
 if (!localStorage.getItem("projectsList")) {
   var projectsList = [];
+  var currentProject;
 
   localStorage.setItem("projectsList", JSON.stringify(projectsList));
-  localStorage.setItem("currentProject", 0);
+  localStorage.setItem("currentProject", currentProject);
 }
 
 document.body.appendChild(display());
