e80419fceb8067412f49a93d412c5e09bdd77170
Hector <baseballhector@gmail.com>
2023-06-26 21:31:19 -0400

    Function to remove a project

---
+++ src/components/project/removeProjectItem.js
@@ -0,0 +1,17 @@
+import { displayUpdatedProjects } from "../../modules/display";
+
+// Removes a project.
+const removeProjectItem = (event) => {
+  var projectsList = JSON.parse(localStorage.getItem("projectsList"));
+
+  var projectToRemove = event.target.parentNode.getAttribute("data-value");
+
+  // Removes selected project.
+  projectsList.splice(projectToRemove, 1);
+
+  // Update localStorage
+  localStorage.setItem("projectsList", JSON.stringify(projectsList));
+  displayUpdatedProjects();
+};
+
+export default removeProjectItem;
