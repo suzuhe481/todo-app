f7c3e693a339145ac9ed5e501e967070b2a8d2bc
Hector <baseballhector@gmail.com>
2023-06-27 14:11:36 -0400

    Improved checks for empty projects and empty todo list

--- src/components/todo/UI/todoContainer.js
+++ src/components/todo/UI/todoContainer.js
@@ -9,16 +9,30 @@ const todoContainer = () => {
   const todoContainer = document.createElement("div");
   todoContainer.id = "todo-container";
 
-  // If todoList in current project is empty, return empty todoContainer.
-  if (projectsList[currentProject].todoList.length == 0) {
+  // Returns empty message if projectsList is undefined or empty.
+  if (projectsList === undefined || projectsList.length == 0) {
+    const todoEmptyMessage = document.createElement("div");
+    todoEmptyMessage.id = "empty-todo-message";
+    todoEmptyMessage.innerHTML =
+      "Create a new project to start making a todo list.";
+    todoContainer.appendChild(todoEmptyMessage);
+
+    return todoContainer;
+  }
+  // Returns empty message if project exists and has no todos,
+  else if (
+    projectsList[currentProject] == undefined ||
+    currentProject == null ||
+    projectsList[currentProject].todoList.length == 0
+  ) {
     const todoEmptyMessage = document.createElement("div");
     todoEmptyMessage.id = "empty-todo-message";
     todoEmptyMessage.innerHTML = "No todos in this project.";
     todoContainer.appendChild(todoEmptyMessage);
 
     return todoContainer;
-  } else {
   }
+
   var todoList = projectsList[currentProject].todoList;
 
   // Appends each todo item to container.
